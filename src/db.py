8437a20bc8b28a41581f968de503a59c2b170874
Connor Rook (Laptop) <cjrook43@gmail.com>
2023-11-18 00:08:23 -0500

    Added database and db manipulation script. FoodTypes.csv out of date but contains all types and associated units

---
+++ src/db.py
@@ -0,0 +1,21 @@
+import csv, sqlite3
+
+def RefreshFoodTypeTable():
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+
+    cur.execute('DROP TABLE IF EXISTS FoodType')
+    
+    cur.execute('CREATE TABLE IF NOT EXISTS FoodType (Type TEXT NOT NULL, Unit TEXT NOT NULL, PRIMARY KEY("Type"));')
+    
+    with open("src/FoodTypes.csv", 'r') as myfile:
+        csv_reader = csv.reader(myfile)
+        for row in csv_reader:
+            try:
+                cur.execute('INSERT INTO FoodType VALUES (?, ?)', (row[0], row[1]))
+            except:
+                pass
+
+
+    con.commit()
+    con.close()
